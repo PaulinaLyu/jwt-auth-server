73e69a8bfae6b59b4321a36f77b4763dffe9a867
Polina Lyubeznova <polina.lyubeznova@geogracom.com>
2024-01-20 11:57:23 +0300

    feature(service/user-service, service/mail-service, controllers/user-controller): Добавила функцию refresh

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -56,7 +56,17 @@ class UserController {
   }
   async refresh(req, res, next) {
     try {
-    } catch (e) {}
+      const { refreshToken } = req.cookie;
+      const userData = await userService.refresh(refreshToken);
+      res.cookie("refreshToken", userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+
+      return res.json(userData);
+    } catch (e) {
+      next(e);
+    }
   }
   async getUsers(req, res, next) {
     try {
